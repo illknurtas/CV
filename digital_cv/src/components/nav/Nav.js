9a6c2a75d16b465f142867cf13fe09f65fa84ec1
illknurtas <77456662+illknurtas@users.noreply.github.com>
2022-11-11 20:02:24 +0300

    bottom nav completed

--- digital_cv/src/components/nav/Nav.js
+++ digital_cv/src/components/nav/Nav.js
@@ -2,27 +2,39 @@ import React from 'react';
 import './nav.css';
 import {HiOutlineHome} from 'react-icons/hi';
 import {AiOutlineUser} from 'react-icons/ai';
-import {CiMail} from 'react-icons/ci';
+import {FiMail} from 'react-icons/fi';
 import {HiOutlineDocumentText} from 'react-icons/hi';
 import {RiServiceLine} from 'react-icons/ri';
+import { useState } from 'react';
 
 const Nav = () => {
+  const [activeNav,setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#">
+      <a href="#" 
+      onClick={()=> setActiveNav('#')}
+      className={activeNav === '#' ? 'active' : ''}>
         <HiOutlineHome/>
       </a>
-      <a href="#about">
+      <a href="#about" 
+      onClick={()=> setActiveNav('#about')}
+      className={activeNav === '#about' ? 'active' : ''}>
         <AiOutlineUser/>
       </a>
-      <a href="#experience">
+      <a href="#experience"
+      onClick={()=> setActiveNav('#experience')}
+      className={activeNav === '#experience' ? 'active' : ''}>
         <HiOutlineDocumentText/>
       </a>
-      <a href="#services">
+      <a href="#services"
+      onClick={()=> setActiveNav('#services')}
+      className={activeNav === '#services' ? 'active' : ''}>
         <RiServiceLine/>
       </a>
-      <a href="#contact">
-        <CiMail/>
+      <a href="#contact"
+      onClick={()=> setActiveNav('#contact')}
+      className={activeNav === '#contact' ? 'active' : ''}>
+        <FiMail/>
       </a>
     </nav>
   )
