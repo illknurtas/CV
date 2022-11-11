595aea204a559f9d728002a5a56b4c48661ef2ed
illknurtas <77456662+illknurtas@users.noreply.github.com>
2022-11-11 23:15:27 +0300

    about and experience components added

--- digital_cv/src/components/about/About.js
+++ digital_cv/src/components/about/About.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './about.css';
-
+import Me from '../../assets/aboutpic.jpg'
+import {FiAward} from 'react-icons/fi'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsFolder} from 'react-icons/bs'
 const About = () => {
   return (
     <section id='about'>
-      About
+      <h2>About Me</h2>
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me_pic'>
+            <img src={Me} alt='about pic'/>
+          </div>
+        </div>
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FiAward/>
+              <h5>Experience</h5>
+              <small>3 months software intern</small>
+            </article>
+            <article className='about_card'>
+              <BsFolder/>
+              <h5>projects</h5>
+              <small>4 projects completed</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae, omnis blanditiis. 
+            Nobis inventore itaque pariatur rerum nisi tempore distinctio, quos obcaecati architecto modi autem 
+            quia dignissimos illum quidem odit consectetur?
+          </p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
     </section>
   )
 }
--- digital_cv/src/components/experience/Experience.js
+++ digital_cv/src/components/experience/Experience.js
@@ -1,10 +1,109 @@
 import React from 'react'
 import './experience.css'
+import {FiCheckSquare} from 'react-icons/fi'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h2>Experiences</h2>
+      <div className='container experience_container'>
+        {/* FRONT-END */}
+        <div className='experience_frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience_content'>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>ReactJS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>VueJS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Beginner</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* BACK-END */}
+        <div className='experience_backend'>
+        <h3>Backend Development</h3>
+          <div className='experience_content'>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>Java</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>Node JS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>RestAPI</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <FiCheckSquare className='experience_details_icon'/>
+              <div>
+                <h4>SQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
